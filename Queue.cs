089d5ba8de98e58fcb3163a9b0f965a44fa1e0e2
tayenphuong <yenphuongtyp24@gmail.com>
2024-11-05 22:41:31 +0700

    Add files via upload

---
+++ Queue.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class ArrayQueue
+    {
+        int[] qArray;
+        int qMax;
+        int qFront;
+        int qRear;
+
+        public ArrayQueue(int MaxItem)
+        {
+            qArray = new int[MaxItem];
+            qMax = MaxItem;
+            qFront = qRear = -1;
+        }
+
+        public int[] QArray { get => qArray; set => qArray = value; }
+        public int QMax { get => qMax; set => qMax = value; }
+        public int QFront { get => qFront; set => qFront = value; }
+        public int QRear { get => qRear; set => qRear = value; }
+
+        public bool IsEmpty => qFront == -1;
+        public bool IsFull => (qRear + 1) % qMax == qFront;
+
+        public bool EnQueue(int newitem)
+        {
+            if (IsFull) return false;
+            if (IsEmpty) qFront = qRear = 0;
+            else
+                qRear = (qRear + 1) % qMax;
+            qArray[qRear] = newitem;
+            return true;
+        }
+        public bool DeQueue(out int itemOut)
+        {
+            itemOut = 0;
+            if (IsEmpty) return false;
+            itemOut = qArray[qFront];
+            if (qFront == qRear)
+                qFront = qRear = -1;
+            else
+                qFront = (qFront + 1) % qMax;
+            return true;
+        }
+        public bool GetFront(out int item)
+        {
+            item = 0;
+            if (IsEmpty) return false;
+            item = qArray[qFront];
+            return true;
+        }
+        public bool GetRear(out int item)
+        {
+            item = 0;
+            if (IsEmpty) return false;
+            item = qArray[qRear];
+            return true;
+        }
+
+    }
+
+
+    class IntNode
+    {
+        private int data;
+        private IntNode next;
+
+        public int Data { get => data; set => data = value; }
+        internal IntNode Next { get => next; set => next = value; }
+        public IntNode(int x = 0)
+        {
+            data = x;
+            next = null;
+        }
+
+    }
+
+
+    class MyList
+    {
+        private IntNode qFront;
+        private IntNode qRear;
+        private int count;
+        public IntNode QFront
+        {
+            get { return qFront; }
+            set { qFront = value; }
+        }
+        public IntNode QRear
+        {
+            get { return qRear; }
+            set { qRear = value; }
+        }
+        public int Count 
+        {
+            get { return count; }
+        }
+
+
+        public MyList() 
+        {
+            qFront = null; 
+            qRear = null;
+            count = 0;
+        }
+        public bool IsEmpty()
+        {
+            return qFront == null;
+        }
+        public bool EnQueue(IntNode newNode)
+        {
+            if (IsEmpty())
+                qFront = qRear = newNode;
+            else
+            {
+                qRear.Next = newNode;
+                qRear = newNode;
+            }
+            count++;
+            return true;
+        }
+       
+        public void ShowList()
+        {
+            int x;
+            while (!IsEmpty()) 
+            {
+                DeQueue(out x);
+                Console.Write($"{x,5}");
+            }
+            Console.WriteLine();
+        }
+
+      
+        public bool DeQueue(out int item)
+        {
+            item = 0;
+            if (IsEmpty()) return false;
+            item = qFront.Data;
+            IntNode pDel = qFront;
+            qFront = qFront.Next;
+            pDel.Next = null;
+            pDel = null;
+            return true;
+        }
+        public bool GetFront(out int outItem)
+        {
+            outItem = 0;
+            if (IsEmpty()) return false;
+            outItem = qFront.Data;
+            return true;
+        }
+        public bool GetRear(out int outItem)
+        {
+            outItem = 0;
+            if (IsEmpty()) return false;
+            outItem = qRear.Data;
+            return true;
+        }
+    }
+
+}
---
+++ Stack.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    class ArrayStack
+    {
+
+        private int[] stkArray;
+        private int stkMax;
+        private int stkTop;
+
+        public int[] StkArray { get => stkArray; set => stkArray = value; }
+        public int StkMax { get => stkMax; set => stkMax = value; }
+        public int StkTop { get => stkTop; set => stkTop = value; }
+
+        public ArrayStack(int max = 0)
+        {
+            stkMax = max;
+            stkArray = new int[max];
+            stkTop = -1;
+        }
+
+        public bool IsEmpty => stkTop == -1;
+
+        public bool IsFull => stkTop == stkMax - 1;
+        public bool Push(int x)
+        {
+            if (IsFull == true) return false;
+            stkTop++;
+            stkArray[stkTop] = x;
+            return true;
+        }
+        public bool Pop(out int outItem)
+        {
+            outItem = default;
+            if (IsEmpty == true) return false;
+            outItem = stkArray[stkTop];
+            stkTop--;
+            return true;
+        }
+        public bool GetTop(out int outItem)
+        {
+            outItem = 0;
+            if (IsEmpty == true) return false;
+            outItem = stkArray[stkTop];
+            return true;
+        }
+
+    }
+
+    class Node
+    {
+        private int data;
+        private Node next;
+        public int Data { get => data; set => data = value; }
+        internal Node Next { get => next; set => next = value; }
+        public Node(int x = 0)
+        {
+            data = x;
+            next = null;
+        }
+
+    }
+    class ListStack
+    {
+        Node top;
+        internal Node Top { get => top; set => top = value; }
+
+        public ListStack()
+        {
+            top = null;
+        }
+
+        public bool IsEmpty => top == null;
+
+        public bool Push(int x)
+        {
+            Node newNode = new Node(x);
+            if (IsEmpty) top = newNode;
+            else
+            {
+                newNode.Next = top;
+                top = newNode;
+            }
+            return true;
+        }
+        public bool Pop(out int outItem)
+        {
+            outItem = 0;
+            if (IsEmpty) return false;
+            outItem = top.Data;
+            Node pDel = top;
+            top = top.Next;
+            pDel.Next = null;
+            pDel = null;
+            return true;
+        }
+        public bool GetTop(out int outItem)
+        {
+            outItem = 0;
+            if (IsEmpty) return false;
+            outItem = top.Data;
+            return true;
+        }
+    }
+
+
+}
